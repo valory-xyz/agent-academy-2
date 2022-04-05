4bbd3c1ee1b0a9aebdcd1712b96584b1183469b6
Philipp Bunke <philippbunke@gmail.com>
2022-04-05 13:11:58 +0200

    Added harvest contract and test

---
+++ packages/philippb/contracts/keep3r_harvest_job/contract.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021 Valory AG
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the scaffold contract definition."""
+
+from typing import Any, cast
+
+from aea.common import JSONLike
+from aea.configurations.base import PublicId
+from aea.contracts.base import Contract
+from aea.crypto.base import LedgerApi
+from aea_ledger_ethereum import EthereumApi
+
+import logging
+
+PUBLIC_ID = PublicId.from_str("philipp/HarvestV2Keep3rStealthJob:0.1.0")
+
+
+CONTRACT_ADDRESS = "0x2150b45626199CFa5089368BDcA30cd0bfB152D6"
+
+class Keep3rHarvestJobContract(Contract):
+    """Implementation of the harvesting job contract."""
+
+    contract_id = PUBLIC_ID
+
+    _logger = logging.getLogger(
+        f"aea.packages.{contract_id.author}.contracts.{contract_id.name}.contract"
+    )
+
+
+    @classmethod
+    def get_reward_multiplier(
+        cls, ledger_api: LedgerApi, contract_address: str
+    ) -> int:
+        ethereum_api = cast(EthereumApi, ledger_api)
+        contract = cls.get_instance(ethereum_api, contract_address)
+        # multiplier = contract.functions.rewardMultiplier().call()
+        multiplier = ethereum_api.contract_method_call(contract, "rewardMultiplier")
+        return multiplier
+
+    
+     
+
+
+    @classmethod
+    def get_raw_transaction(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> JSONLike:
+        """
+        Handler method for the 'GET_RAW_TRANSACTION' requests.
+
+        Implement this method in the sub class if you want
+        to handle the contract requests manually.
+
+        :param ledger_api: the ledger apis.
+        :param contract_address: the contract address.
+        :param kwargs: the keyword arguments.
+        :return: the tx  # noqa: DAR202
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_raw_message(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> bytes:
+        """
+        Handler method for the 'GET_RAW_MESSAGE' requests.
+
+        Implement this method in the sub class if you want
+        to handle the contract requests manually.
+
+        :param ledger_api: the ledger apis.
+        :param contract_address: the contract address.
+        :param kwargs: the keyword arguments.
+        :return: the tx  # noqa: DAR202
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_state(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> JSONLike:
+        """
+        Handler method for the 'GET_STATE' requests.
+
+        Implement this method in the sub class if you want
+        to handle the contract requests manually.
+
+        :param ledger_api: the ledger apis.
+        :param contract_address: the contract address.
+        :param kwargs: the keyword arguments.
+        :return: the tx  # noqa: DAR202
+        """
+        raise NotImplementedError
--- tests/test_packages/test_contracts/test_harvest_contract.py
+++ tests/test_packages/test_contracts/test_harvest_contract.py
@@ -24,74 +24,18 @@ from unittest import mock
 
 from aea.test_tools.test_contract import BaseContractTestCase
 
-from packages.gabrielfu.contracts.keep3r_job.contract import (
-    CONTRACT_ADDRESS,
-    Keep3rJobContract,
-    PUBLIC_ID,
-)
-
 from packages.philippb.contracts.keep3r_harvest_job.contract import (
     CONTRACT_ADDRESS as HARVEST_CONTRACT_ADDRESS,
-    Keep3rJobContract as Keep3rHarvestJobContract,
+    Keep3rHarvestJobContract,
     PUBLIC_ID as HARVEST_PUBLIC_ID
 )
 
 from tests.conftest import ROOT_DIR
 
 
-CHAIN_ID = 1
-
-GAS_PRICE = 20000000000
 IS_WORKABLE = True
 
 REWARD_MULTIPLIER = 975
-
-class TestKeep3rJobContract(BaseContractTestCase):
-    """Test Keep3rJobContract."""
-
-    path_to_contract = Path(
-        ROOT_DIR, "packages", PUBLIC_ID.author, "contracts", PUBLIC_ID.name
-    )
-    ledger_identifier = "ethereum"
-    contract: Keep3rJobContract
-
-    @classmethod
-    def finish_contract_deployment(cls) -> str:
-        """Finish the contract deployment."""
-        contract_address = CONTRACT_ADDRESS
-        return contract_address
-
-    @classmethod
-    def _deploy_contract(cls, contract, ledger_api, deployer_crypto, gas) -> Dict:  # type: ignore
-        """Deploy contract."""
-        return {}
-
-    def test_get_gas_price(self) -> None:
-        """Test gets gas price."""
-        with mock.patch.object(
-            self.ledger_api.api.eth, "generate_gas_price", return_value=GAS_PRICE
-        ):
-            with mock.patch.object(
-                self.ledger_api.api.manager, "request_blocking", return_value=CHAIN_ID
-            ):
-                result = self.contract.get_gas_price(self.ledger_api)
-        assert result == GAS_PRICE
-
-    def test_get_workable(self) -> None:
-        """Test gets `workable` method."""
-        # mock contract function
-        mock_function = mock.MagicMock()
-        mock_function.call.return_value = IS_WORKABLE
-        # mock contract instance
-        mock_instance = mock.MagicMock()
-        mock_instance.functions.workable.return_value = mock_function
-
-        with mock.patch.object(
-            self.ledger_api, "get_contract_instance", return_value=mock_instance
-        ):
-            result = self.contract.get_workable(self.ledger_api, CONTRACT_ADDRESS)
-        assert result == IS_WORKABLE
-
 
 class TestKeep3rHarvestJobContract(BaseContractTestCase):
     """Test Keep3r Harvest Job Contract."""
@@ -126,4 +70,3 @@ class TestKeep3rHarvestJobContract(BaseContractTestCase):
         ):
             result = self.contract.get_reward_multiplier(self.ledger_api, HARVEST_CONTRACT_ADDRESS)
         assert result == REWARD_MULTIPLIER
-
