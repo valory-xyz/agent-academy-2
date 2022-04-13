74c5c723ea748dc247f326c1d77aa739480377a2
Gabriel Fu <hfu.gabriel@gmail.com>
2022-04-13 17:45:53 +0800

    add agent

---
+++ tests/test_packages/test_agents/test_keep3r_bot/test_agent.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021-2022 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""End2end tests for the keep3r_co/keep3r_abci skill."""
+
+import pytest
+
+from tests.test_packages.test_agents.base import BaseTestEnd2EndNormalExecution
+
+
+# check log messages of the happy path
+CHECK_STRINGS = (
+    "Entered in the 'registration' round for period 0",
+    "'registration' round is done",
+    "Entered in the 'randomness_startup' round for period 0",
+    "'randomness_startup' round is done",
+    "Entered in the 'select_keeper_at_startup' round for period 0",
+    "'select_keeper_at_startup' round is done",
+    "Entered in the 'reset_and_pause' round for period 0",
+    "'reset_and_pause' round is done",
+    "Period end",
+    "Entered in the 'randomness_startup' round for period 1",
+    "Entered in the 'select_keeper_at_startup' round for period 1",
+    "Entered in the 'reset_and_pause' round for period 1",
+)
+
+
+@pytest.mark.e2e
+class TestKeep3rABCISingleAgent(
+    BaseTestEnd2EndNormalExecution,
+):
+    """Test that the ABCI keep3r_abci skill with only one agent."""
+
+    NB_AGENTS = 1
+    agent_package = "keep3r_co/keep3r_bot:0.1.0"
+    skill_package = "keep3r_co/keep3r_abci:0.1.0"
+    wait_to_finish = 40
+    check_strings = CHECK_STRINGS
+
+
+@pytest.mark.e2e
+class TestKeep3rABCITwoAgents(
+    BaseTestEnd2EndNormalExecution,
+):
+    """Test that the ABCI keep3r_abci skill with two agents."""
+
+    NB_AGENTS = 2
+    agent_package = "keep3r_co/keep3r_bot:0.1.0"
+    skill_package = "keep3r_co/keep3r_abci:0.1.0"
+    wait_to_finish = 80
+    check_strings = CHECK_STRINGS
+
+
+@pytest.mark.e2e
+class TestKeep3rABCIFourAgents(
+    BaseTestEnd2EndNormalExecution,
+):
+    """Test that the ABCI keep3r_abci skill with four agents."""
+
+    NB_AGENTS = 4
+    agent_package = "keep3r_co/keep3r_bot:0.1.0"
+    skill_package = "keep3r_co/keep3r_abci:0.1.0"
+    wait_to_finish = 80
+    check_strings = CHECK_STRINGS
