e30dd8b2cbb22cbdc05644c56d4fbaeb289887e9
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-08-17 12:37:30 +0200

    tests: add constants.py

--- tests/test_contracts/test_keep3r_v1/test_contract.py
+++ tests/test_contracts/test_keep3r_v1/test_contract.py
@@ -20,37 +20,31 @@
 """Tests for the keep3r v1 contract."""
 
 from pathlib import Path
-from typing import Dict, Any, cast
+from typing import Any, Dict, cast
 
+from aea_ledger_ethereum import EthereumApi, EthereumCrypto
 from autonomy.test_tools.base_test_classes.contracts import (
     BaseGanacheContractWithDependencyTest,
 )
-from web3 import Web3
-from aea_ledger_ethereum import EthereumCrypto, EthereumApi
-
 from autonomy.test_tools.docker.base import skip_docker_tests
+from web3 import Web3
 
-from packages.valory.contracts.keep3r_v1_library.contract import PUBLIC_ID as LIBRARY_PUBLIC_ID
-from packages.valory.contracts.keep3r_v1.contract import (
-    PUBLIC_ID,
-    Keep3rV1Contract,
+from packages.valory.contracts.keep3r_v1.contract import Keep3rV1Contract, PUBLIC_ID
+from packages.valory.contracts.keep3r_v1_library.contract import (
+    PUBLIC_ID as LIBRARY_PUBLIC_ID,
 )
 
 from tests.conftest import ROOT_DIR
+from tests.test_contracts.constants import DEFAULT_GAS
 
 
-DEFAULT_GAS = 10000000
-DEFAULT_MAX_FEE_PER_GAS = 10 ** 10
-DEFAULT_MAX_PRIORITY_FEE_PER_GAS = 10 ** 10
 DEPLOYER = Web3.toChecksumAddress("0x1fdf6a150ffc90dcc61c05d9b641a242fcc37823")
-
 BASE_CONTRACT_PATH = Path(ROOT_DIR, "packages", PUBLIC_ID.author, "contracts")
 
 
 class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
     """Base class for the Keep3rV1 contract tests"""
 
-    contract: Keep3rV1Contract
     contract_address = Keep3rV1Contract.address
     contract_directory = BASE_CONTRACT_PATH / PUBLIC_ID.name
 
@@ -58,12 +52,12 @@ class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
     ledger_identifier = EthereumCrypto.identifier
 
     dependencies = [
-                       (
-                           LIBRARY_PUBLIC_ID.name,
-                           Path(BASE_CONTRACT_PATH / LIBRARY_PUBLIC_ID.name),
-                           dict(gas=DEFAULT_GAS),
-                       ),
-                    ]
+        (
+            LIBRARY_PUBLIC_ID.name,
+            Path(BASE_CONTRACT_PATH / LIBRARY_PUBLIC_ID.name),
+            dict(gas=DEFAULT_GAS),
+        ),
+    ]
 
     @classmethod
     def deployment_kwargs(cls) -> Dict[str, Any]:
@@ -72,7 +66,7 @@ class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
         return dict(gas=DEFAULT_GAS, _kph=DEPLOYER)
 
     @property
-    def contract(self) -> Keep3rV1Contract:
+    def contract(self) -> Keep3rV1Contract:  # type: ignore
         """Get the contract."""
         return cast(Keep3rV1Contract, super().contract)
 
@@ -81,7 +75,7 @@ class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
 class TestKeep3rV1Contract(BaseKeep3rV1ContractTest):
     """Test Keep3r V1 Contract"""
 
-    def test_dependencies_deployed(self):
+    def test_dependencies_deployed(self) -> None:
         """Test contract dependencies are successfully deployed"""
 
         assert self.dependency_info
