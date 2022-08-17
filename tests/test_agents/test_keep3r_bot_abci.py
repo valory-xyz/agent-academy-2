4909ee094e85bece1cabc43d8618867662a08969
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-08-17 12:43:50 +0200

    fix: use @pytest.mark.skip

--- tests/test_agents/test_keep3r_bot_abci.py
+++ tests/test_agents/test_keep3r_bot_abci.py
@@ -24,9 +24,6 @@ from autonomy.test_tools.base_test_classes.agents import BaseTestEnd2EndExecutio
 from autonomy.test_tools.fixture_helpers import UseGnosisSafeHardHatNet
 
 
-pytest.skip(allow_module_level=True)
-
-
 # check log messages of the happy path
 CHECK_STRINGS = (
     "Entered in the 'registration_startup' round for period 0",
@@ -53,16 +50,19 @@ class BaseKeep3rABCITest(BaseTestEnd2EndExecution, UseGnosisSafeHardHatNet):
     use_benchmarks = True
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("nb_nodes", (1,))
 class TestKeep3rABCISingleAgent(BaseKeep3rABCITest):
     """Test that the ABCI keep3r_abci skill with only one agent."""
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("nb_nodes", (2,))
 class TestKeep3rABCITwoAgents(BaseKeep3rABCITest):
     """Test that the ABCI keep3r_abci skill with two agents."""
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("nb_nodes", (4,))
 class TestKeep3rABCIFourAgents(BaseKeep3rABCITest):
     """Test that the ABCI keep3r_abci skill with four agents."""
