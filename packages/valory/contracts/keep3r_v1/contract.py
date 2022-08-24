fd1b7589393f4b11e20aba9e8e96ebf7fcf68d67
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-08-24 13:19:12 +0200

    docs: update docstring build_approve_tx

--- packages/valory/contracts/keep3r_v1/contract.py
+++ packages/valory/contracts/keep3r_v1/contract.py
@@ -111,7 +111,19 @@ class Keep3rV1Contract(Contract):
         address: str,
         amount: Wei,
     ) -> RawTransaction:
-        """Allows a keeper to activate/register themselves after bonding."""
+        """Allows a keeper to activate/register themselves after bonding.
+
+        Sets `amount` as the allowance of `spender` over the caller's tokens.
+
+        Returns a boolean value indicating whether the operation succeeded.
+
+        IMPORTANT: Beware that changing an allowance with this method brings the risk
+        that someone may use both the old and the new allowance by unfortunate
+        transaction ordering. One possible solution to mitigate this race
+        condition is to first reduce the spender's allowance to 0 and set the
+        desired value afterwards:
+        https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        """
 
         contract = cls.get_instance(ledger_api, contract_address)
         function = contract.functions.approve(spender=contract.address, amount=amount)
