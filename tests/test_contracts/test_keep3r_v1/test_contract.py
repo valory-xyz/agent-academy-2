05a09592147435e1fa5e6bdb04263818cf83f74d
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-08-25 02:03:22 +0200

    tests: KeeperV1 with TestJob

--- tests/test_contracts/test_keep3r_v1/test_contract.py
+++ tests/test_contracts/test_keep3r_v1/test_contract.py
@@ -19,23 +19,30 @@
 
 """Tests for the keep3r v1 contract."""
 
+import json
+import time
 from pathlib import Path
 from typing import Any, Dict, cast
 
+from aea.common import JSONLike
 from aea_ledger_ethereum import EthereumApi, EthereumCrypto
+from web3.types import Nonce, TxParams, Wei
 
 from autonomy.test_tools.base_test_classes.contracts import (
     BaseGanacheContractWithDependencyTest,
 )
 from autonomy.test_tools.docker.base import skip_docker_tests
 
+from packages.valory.contracts.keep3r_test_job.contract import (
+    PUBLIC_ID as TEST_JOB_PUBLIC_ID,
+)
 from packages.valory.contracts.keep3r_v1.contract import Keep3rV1Contract, PUBLIC_ID
 from packages.valory.contracts.keep3r_v1_library.contract import (
     PUBLIC_ID as LIBRARY_PUBLIC_ID,
 )
 
-from tests.conftest import KEEP3R_HELPER_FOR_TEST, KEEP3R_V1_FOR_TEST, ROOT_DIR
-from tests.test_contracts.constants import DEFAULT_GAS, ONE_ETH
+from tests.conftest import KEEP3R_V1_FOR_TEST, ROOT_DIR
+from tests.test_contracts.constants import DEFAULT_GAS, HALF_A_SECOND, ONE_ETH
 
 
 BASE_CONTRACT_PATH = Path(ROOT_DIR, "packages", PUBLIC_ID.author, "contracts")
@@ -62,7 +69,7 @@ class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
     def deployment_kwargs(cls) -> Dict[str, Any]:
         """Get deployment kwargs."""
 
-        return dict(gas=DEFAULT_GAS, _kph=KEEP3R_HELPER_FOR_TEST)
+        return dict(gas=DEFAULT_GAS, _kph=cls.deployer_crypto.address)
 
     @property
     def contract(self) -> Keep3rV1Contract:  # type: ignore
@@ -71,14 +78,8 @@ class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
         return cast(Keep3rV1Contract, super().contract)
 
     @property
-    def alice_address(self) -> str:
-        """Alice's wallet; funded with 1000 ETH"""
-
-        return self.key_pairs()[0][0]
-
-    @property
-    def bob_address(self) -> str:
-        """Bob's wallet; empty"""
+    def empty_address(self) -> str:
+        """Empty wallet address"""
 
         return "0x1B621c19C3E868A4DF2E1858b08cedA8633927EA"
 
@@ -87,6 +88,45 @@ class BaseKeep3rV1ContractTest(BaseGanacheContractWithDependencyTest):
         """Keyword arguments expected by every method call"""
 
         return dict(ledger_api=self.ledger_api, contract_address=self.contract_address)
+
+    @staticmethod
+    def get_tx_parameters(ledger_api: EthereumApi, address: str) -> TxParams:
+        """Get transaction parameters."""
+
+        tx_parameters = TxParams()
+        nonce = Nonce(ledger_api.api.eth.get_transaction_count(address))
+        tx_parameters["from"] = ledger_api.api.toChecksumAddress(address)
+        tx_parameters["nonce"] = nonce
+        tx_parameters["gas"] = Wei(DEFAULT_GAS)
+        tx_parameters.update(ledger_api.try_get_gas_pricing())
+        return tx_parameters
+
+    @classmethod
+    def perform_tx(cls, raw_tx: Dict[str, Any]) -> JSONLike:
+        """Perform the transaction"""
+
+        signed_tx = cls.deployer_crypto.sign_transaction(raw_tx)
+        tx_digest = cls.ledger_api.send_signed_transaction(signed_tx)
+        time.sleep(HALF_A_SECOND)
+        tx_receipt = cls.ledger_api.get_transaction_receipt(tx_digest)
+        time.sleep(HALF_A_SECOND)
+        if not tx_receipt or not tx_receipt["status"]:
+            raise ValueError(f"transaction failed: {tx_receipt}")
+        return tx_receipt
+
+    @classmethod
+    def deploy_contract(cls, path: Path, **kwargs: Any) -> Any:
+        """Deploy (additional) contract"""
+
+        json_files = list(path.glob("build/*.json"))
+        assert len(json_files) == 1, json_files
+        interface = json.loads(json_files.pop().read_text(encoding="utf-8"))
+        raw_tx = cls.ledger_api.get_deploy_transaction(
+            interface, cls.deployer_crypto.address, **kwargs
+        )
+        tx_receipt = cls.perform_tx(raw_tx)
+        contract_address = str(tx_receipt["contractAddress"])
+        return cls.ledger_api.get_contract_instance(interface, contract_address)
 
 
 @skip_docker_tests
@@ -111,13 +151,17 @@ class TestKeep3rV1Contract(BaseKeep3rV1ContractTest):
     def test_is_keeper(self) -> None:
         """Test is_keeper"""
 
-        kw = dict(address=self.alice_address)
+        kw = dict(address=self.deployer_crypto.address)
         assert self.contract.is_keeper(**self.base_kw, **kw) is False
 
     def test_build_approve_tx(self) -> None:
         """Test get_jobs"""
 
-        kw = dict(address=self.alice_address, spender=self.bob_address, amount=ONE_ETH)
+        kw = dict(
+            address=self.deployer_crypto.address,
+            spender=self.empty_address,
+            amount=ONE_ETH,
+        )
         raw_tx = self.contract.build_approve_tx(**self.base_kw, **kw)  # type: ignore
         expected = "0x095ea7b30000000000000000000000001b621c19c3e868a4df2e1858b08ceda8633927ea0000000000000000000000000000000000000000000000000de0b6b3a7640000"
         assert raw_tx["data"] == expected
@@ -125,13 +169,13 @@ class TestKeep3rV1Contract(BaseKeep3rV1ContractTest):
     def test_allowance(self) -> None:
         """Test allowance"""
 
-        kw = dict(account=self.alice_address, spender=self.bob_address)
+        kw = dict(address=self.deployer_crypto.address, spender=self.empty_address)
         assert self.contract.allowance(**self.base_kw, **kw) == 0
 
     def test_build_bond_tx(self) -> None:
         """Test build_bond_tx"""
 
-        kw = dict(address=self.bob_address, amount=ONE_ETH)
+        kw = dict(address=self.empty_address, amount=ONE_ETH)
         raw_tx = self.contract.build_bond_tx(**self.base_kw, **kw)  # type: ignore
         expected = "0xa515366a000000000000000000000000e7f1725e7734ce288f8367e1bb143e90bb3f05120000000000000000000000000000000000000000000000000de0b6b3a7640000"
         assert raw_tx["data"] == expected
@@ -139,7 +183,7 @@ class TestKeep3rV1Contract(BaseKeep3rV1ContractTest):
     def test_build_activate_tx(self) -> None:
         """Test build_activate_tx"""
 
-        kw = dict(address=self.alice_address)
+        kw = dict(address=self.deployer_crypto.address)
         raw_tx = self.contract.build_activate_tx(**self.base_kw, **kw)
         expected = (
             "0x1c5a9d9c000000000000000000000000e7f1725e7734ce288f8367e1bb143e90bb3f0512"
@@ -149,7 +193,7 @@ class TestKeep3rV1Contract(BaseKeep3rV1ContractTest):
     def test_build_unbond_tx(self) -> None:
         """Test build_unbond_tx"""
 
-        kw = dict(address=self.bob_address, amount=ONE_ETH)
+        kw = dict(address=self.empty_address, amount=ONE_ETH)
         raw_tx = self.contract.build_unbond_tx(**self.base_kw, **kw)  # type: ignore
         expected = "0xa5d059ca000000000000000000000000e7f1725e7734ce288f8367e1bb143e90bb3f05120000000000000000000000000000000000000000000000000de0b6b3a7640000"
         assert raw_tx["data"] == expected
@@ -157,9 +201,83 @@ class TestKeep3rV1Contract(BaseKeep3rV1ContractTest):
     def test_build_withdraw_tx(self) -> None:
         """Test build_withdraw_tx"""
 
-        kw = dict(address=self.alice_address)
+        kw = dict(address=self.deployer_crypto.address)
         raw_tx = self.contract.build_withdraw_tx(**self.base_kw, **kw)
         expected = (
             "0x51cff8d9000000000000000000000000e7f1725e7734ce288f8367e1bb143e90bb3f0512"
         )
         assert raw_tx["data"] == expected
+
+
+@skip_docker_tests
+class TestKeep3rV1ContractWithTestJob(BaseKeep3rV1ContractTest):
+    """Test Keep3r V1 Contract in conjunction with the TestJob contract."""
+
+    job_path = Path(BASE_CONTRACT_PATH / TEST_JOB_PUBLIC_ID.name)
+    test_job_contract: Any
+
+    def add_job(self, contract_address: str) -> JSONLike:
+        """Add job."""
+
+        contract = self.contract.get_instance(**self.base_kw)  # type: ignore
+        tx_parameters = self.get_tx_parameters(
+            self.ledger_api, self.deployer_crypto.address
+        )
+        function = contract.functions.addJob(job=contract_address)
+        raw_tx = function.buildTransaction(tx_parameters)
+        return self.perform_tx(raw_tx)
+
+    @classmethod
+    def setup_class(cls) -> None:
+        """Setup class"""
+
+        super().setup_class()
+        kwargs = dict(gas=DEFAULT_GAS, _keep3r=cls.deployer_crypto.address)
+        cls.test_job_contract = cls.deploy_contract(cls.job_path, **kwargs)
+
+    def test_keep3r_test_job_contract_deployed(self) -> None:
+        """Test Keep3r TestJob contract is deployed"""
+
+        assert self.test_job_contract
+
+    def test_add_and_get_jobs(self) -> None:
+        """Test get_jobs after adding the test job contract."""
+
+        self.add_job(self.test_job_contract.address)
+        expected = [self.test_job_contract.address]
+        assert self.contract.get_jobs(**self.base_kw) == expected
+
+    def wait_n_blocks(self, n: int = 1) -> None:
+        """Wait for n blocks."""
+
+        target = self.ledger_api.api.eth.get_block_number() + n
+        while (current := self.ledger_api.api.eth.get_block_number()) < target:
+            print(f"waiting for more blocks, current: {current}, target: {target}")
+            time.sleep(3)
+
+    def test_become_keeper(self) -> None:
+        """Test become keeper"""
+
+        amount = 0
+        kw = dict(address=self.deployer_crypto.address)
+        assert self.contract.is_keeper(**self.base_kw, **kw) is False
+
+        # 1. approve
+        raw_tx = self.contract.build_approve_tx(
+            **self.base_kw, **kw, spender=self.contract_address, amount=amount
+        )
+        self.perform_tx(raw_tx)
+
+        # 2. bond - normally has a bonding period associated
+        raw_tx = self.contract.build_bond_tx(**self.base_kw, **kw, amount=amount)
+        raw_tx["gas"] *= 100  # TODO: works for now
+        self.perform_tx(raw_tx)
+
+        # 3. activate
+        # TODO: need to advance time and block here somehow
+        raw_tx = self.contract.build_activate_tx(**self.base_kw, **kw)
+        self.perform_tx(raw_tx)
+
+        # validate
+        kw = dict(address=self.deployer_crypto.address)
+        assert self.contract.is_keeper(**self.base_kw, **kw) is True
