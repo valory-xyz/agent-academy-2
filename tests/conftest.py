7cbe0d3a5efa59077c1b2c16a843de490b9c0483
David Minarsch <david.minarsch@googlemail.com>
2022-09-15 13:51:09 +0100

    fix: mypy issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,6 @@ from aea_test_autonomy.docker.base import launch_image
 from aea_test_autonomy.docker.gnosis_safe_net import (
     DEFAULT_HARDHAT_ADDR,
     DEFAULT_HARDHAT_PORT,
-    GnosisSafeNetDockerImage,
 )
 
 from tests.helpers.docker.keep3r_net import Keep3rNetDockerImage
@@ -41,13 +40,6 @@ from tests.helpers.docker.keep3r_net import Keep3rNetDockerImage
 CUR_PATH = os.path.dirname(inspect.getfile(inspect.currentframe()))  # type: ignore
 ROOT_DIR = Path(CUR_PATH, "..").resolve().absolute()
 THIRD_PARTY_CONTRACTS = ROOT_DIR / "third_party"
-
-
-@pytest.fixture()
-def key_pairs() -> List[Tuple[str, str]]:
-    """Get the default key paris for hardhat."""
-    return KEY_PAIRS
-
 
 # Keep3rV1ForTest - Goerli
 KEEP3R_V1_FOR_TEST = "0x3364BF0a8DcB15E463E6659175c90A57ee3d4288"
@@ -58,34 +50,6 @@ KEEP3R_TEST_JOB = "0xd50345ca88e0B2cF9a6f5eD29C1F1f9d76A16C3c"
 KEEP3R_V1 = "0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44"
 KEEP3R_HELPER = "0xb41772890c8b1564c5015a12c0dc6f18b0af955e"
 KEEP3R_V1_LIBRARY = "0xfc38B6eBA9d47CBFc8C7B4FFfFd142B78996B6f1"
-
-
-@pytest.fixture()
-def hardhat_addr() -> str:
-    """Get the hardhat addr"""
-    return DEFAULT_HARDHAT_ADDR
-
-
-@pytest.fixture()
-def hardhat_port() -> int:
-    """Get the hardhat port"""
-    return DEFAULT_HARDHAT_PORT
-
-
-@pytest.fixture(scope="function")
-def gnosis_safe_hardhat_scope_function(
-    hardhat_addr: Any,
-    hardhat_port: Any,
-    timeout: float = 3.0,
-    max_attempts: int = 40,
-) -> Generator:
-    """Launch the HardHat node with Gnosis Safe contracts deployed. This fixture is scoped to a function which means it will destroyed at the end of the test."""
-    client = docker.from_env()
-    logging.info(f"Launching Hardhat at port {hardhat_port}")
-    image = GnosisSafeNetDockerImage(
-        client, THIRD_PARTY_CONTRACTS, hardhat_addr, hardhat_port
-    )
-    yield from launch_image(image, timeout=timeout, max_attempts=max_attempts)
 
 
 @pytest.fixture()
