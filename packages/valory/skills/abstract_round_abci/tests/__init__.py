8fa46f704f4375de1552eb1c0833b4bb6bcec534
Ardian <ardianabazi09@gmail.com>
2023-10-23 14:48:22 +0200

    fix: deps versions

--- packages/valory/skills/abstract_round_abci/tests/__init__.py
+++ packages/valory/skills/abstract_round_abci/tests/__init__.py
@@ -18,3 +18,6 @@
 # ------------------------------------------------------------------------------
 
 """Tests for valory/abstract_round_abci skill."""
+from hypothesis import settings  # pragma: nocover
+
+settings.register_profile(CI, deadline=5000)  # pragma: nocover
